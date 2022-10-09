43c0f40339113ae90c142a0a6ec014f476908bea
Yves <robertgherghe0103@gmail.com>
2022-10-09 20:59:46 +0300

    Creating Stores + ViewModels

---
+++ YouTubeViewers.WPF/ViewModels/YouTubeViewersViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace YouTubeViewers.WPF.ViewModels
+{
+    public class YouTubeViewersViewModel : ViewModelBase
+    {
+        public YoutubeViewersListingViewModel YoutubeViewersListingViewModel { get; }
+        public YouTubeViewersDetailsViewModel YouTubeViewersDetailsViewModel { get; }
+
+        public ICommand AddYoTubeViewrsCommand { get; }
+
+        public YouTubeViewersViewModel()
+        {
+            YoutubeViewersListingViewModel = new YoutubeViewersListingViewModel();
+            YouTubeViewersDetailsViewModel = new YouTubeViewersDetailsViewModel();
+        }
+    }
+}
